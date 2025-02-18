41087e4b90e2aef2ab561e41cc62f37836c8f7a6
NaufalArdian12 <naufalopo87@gmail.com>
2025-02-18 18:59:59 +0700

    feat(views): add routes for landing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LandingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('testimonials', [LandingController::class, 'testimonials'])->name('landing.testimonials');
+Route::get('wishlist', [LandingController::class, 'wishlist'])->name('landing.wishlist');
+Route::get('details/{slug}', [LandingController::class, 'details'])->name('landing.details');
+Route::resource('/', LandingController::class);
